c976599a36ff8b13ab38d02bbe8051b4470a0541
Tomasz Rakowski <tomasz@akayami.com>
2016-01-17 17:09:51 -0500

    Reorganized few things

--- index.js
+++ index.js
@@ -1,101 +1,36 @@
-var async = require('async');
+var generator = require('./lib/engine.js');
 var fs = require('fs');
 
-module.exports = function(conn, dbname, structSQL) {
-	var routine = [
-		function(mainCB) {
-			async.waterfall([
-				function(asyncCB) {
-					conn.query('DROP DATABASE IF EXISTS ??', [dbname], function(err, result) {
-						if (err) {
-							asyncCB(err);
-						} else {
-							asyncCB(null, conn);
-						}
-					})
-				},
-				function(conn, asyncCB) {
-					conn.query('CREATE DATABASE IF NOT EXISTS ??', [dbname], function(err, result) {
-						if (err) {
-							asyncCB(err);
-						} else {
-							asyncCB(null, conn, result);
-						}
-					})
-				},
-				function(conn, result, asyncCB) {
-					conn.query('USE ??', [dbname], function(err, result) {
-						if (err) {
-							asyncCB(err);
-						} else {
-							asyncCB(null, conn, result);
-
-						}
-					})
-				},
-				function(conn, result, asyncCB) {
-					conn.query(structSQL, function(err, result) {
-						if (err) {
-							asyncCB(err);
-						} else {
-							asyncCB(null, conn, result);
-
-						}
-					})
-				},
-			], function(err, result) {
-				if (err) {
-					mainCB(err);
-				} else {
-					mainCB(null, result);
-				}
-			});
-		},
-		function(conn, mainCB) { // Select appropraite db
-			conn.query('USE ??', [dbname], function(err, result) {
-				if (err) {
-					mainCB(err);
-				} else {
-					mainCB(null, conn);
-				}
-			})
-		}
-	]
-
+module.exports = function(options) {
 	return {
-		make: function(data) {
-			data.forEach(function(block) {
-				for (var table in block) {
-					block[table].forEach(function(item) {
-						routine.push(function(conn, cb) {
-							for (var key in item.input) {
-								if (typeof item.input[key] == 'function') {
-									item.input[key] = item.input[key](data);
-								}
-							}
-							conn.insert(table, item.input, function(err, result) {
-								if (err) {
-									cb(err);
-								} else {
-									item.result = {
-										insertId: result.insertId
-									}
-									cb(null, conn);
-								}
-							})
-						})
-					})
-				}
-			});
-		},
-
-		run: function(cb) {
-			async.waterfall(routine, function(err, result) {
+		create: function() {
+			options.conn.connect(function(err) {
 				if (err) {
-					cb(err, null);
-				} else {
-					cb(null, result);
+					console.error('error connecting: ' + err.stack);
+					return;
 				}
+				fs.readFile(options.structureFile, function(err, sql) {
+					if (err) {
+						console.error('error connecting: ' + err.stack);
+						return;
+					}
+					var gen = generator(options.conn, options.schemaName, sql.toString());
+					var data = require(options.dataFile);
+					gen.make(data);
+					gen.run(function(err, result) {
+						if (err) {
+							console.log(err);
+							console.log(err.stack);
+							connection.close();
+							return;
+						}
+						options.conn.query('DROP DATABASE IF EXISTS ??', [options.schemaName], function(err, result) {
+							console.log('Test Data Cleaned Up')
+							console.log('Done');
+							options.conn.close();
+						})
+					});
+				});
 			});
 		}
 	}
