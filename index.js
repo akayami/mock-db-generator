556c4edb4ddfc67842f7299443376a544ba2a0e8
Tomasz Rakowski <tomasz@akayami.com>
2016-01-17 17:21:36 -0500

    more changes

--- index.js
+++ index.js
@@ -3,28 +3,17 @@ var fs = require('fs');
 
 module.exports = function(options) {
 	return {
-		create: function() {
-			options.conn.connect(function(err) {
+		create: function(cb) {
+			fs.readFile(options.structureFile, function(err, sql) {
 				if (err) {
 					console.error('error connecting: ' + err.stack);
 					return;
 				}
-				fs.readFile(options.structureFile, function(err, sql) {
-					if (err) {
-						console.error('error connecting: ' + err.stack);
-						return;
-					}
-					var gen = generator(options.conn, options.schemaName, sql.toString());
-					var data = require(options.dataFile);
-					gen.make(data);
-					gen.run(function(err, result) {
-						if (err) {
-							console.log(err);
-							console.log(err.stack);
-							connection.close();
-							return;
-						}
-					});
+				var gen = generator(options.conn, options.schemaName, sql.toString());
+				var data = require(options.dataFile);
+				gen.make(data);
+				gen.run(function(err, result) {
+					cb(err);
 				});
 			});
 		}
